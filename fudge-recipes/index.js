f16e5684c16d7a1c9b08880ec679188a77cf7195
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-12-04 18:00:31 +0000

    added files for fudge recipe how to skill

---
+++ fudge-recipes/index.js
@@ -0,0 +1,251 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+const recipes = require('./recipes');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+/* INTENT HANDLERS */
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+    const item = requestAttributes.t(getRandomItem(Object.keys(recipes.RECIPE_EN_US)));
+
+    const speakOutput = requestAttributes.t('WELCOME_MESSAGE', requestAttributes.t('SKILL_NAME'), item);
+    const repromptOutput = requestAttributes.t('WELCOME_REPROMPT');
+
+    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(repromptOutput)
+      .getResponse();
+  },
+};
+
+const RecipeHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'RecipeIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+    const itemSlot = handlerInput.requestEnvelope.request.intent.slots.Item;
+    let itemName;
+    if (itemSlot && itemSlot.value) {
+      itemName = itemSlot.value.toLowerCase();
+    }
+
+    const cardTitle = requestAttributes.t('DISPLAY_CARD_TITLE', requestAttributes.t('SKILL_NAME'), itemName);
+    const myRecipes = requestAttributes.t('RECIPES');
+    const recipe = myRecipes[itemName];
+    let speakOutput = "";
+
+    if (recipe) {
+      sessionAttributes.speakOutput = recipe;
+      //sessionAttributes.repromptSpeech = requestAttributes.t('RECIPE_REPEAT_MESSAGE');
+      handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+      return handlerInput.responseBuilder
+        .speak(sessionAttributes.speakOutput) // .reprompt(sessionAttributes.repromptSpeech)
+        .withSimpleCard(cardTitle, recipe)
+        .getResponse();
+    }
+    else{
+      speakOutput = requestAttributes.t('RECIPE_NOT_FOUND_MESSAGE');
+      const repromptSpeech = requestAttributes.t('RECIPE_NOT_FOUND_REPROMPT');
+      if (itemName) {
+        speakOutput += requestAttributes.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
+      } else {
+        speakOutput += requestAttributes.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
+      }
+      speakOutput += repromptSpeech;
+
+      sessionAttributes.speakOutput = speakOutput; //saving speakOutput to attributes, so we can use it to repeat
+      sessionAttributes.repromptSpeech = repromptSpeech;
+
+      handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+      return handlerInput.responseBuilder
+        .speak(sessionAttributes.speakOutput)
+        .reprompt(sessionAttributes.repromptSpeech)
+        .getResponse();
+    }
+  }
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+    const item = requestAttributes.t(getRandomItem(Object.keys(recipes.RECIPE_EN_US)));
+
+    sessionAttributes.speakOutput = requestAttributes.t('HELP_MESSAGE', item);
+    sessionAttributes.repromptSpeech = requestAttributes.t('HELP_REPROMPT', item);
+
+    return handlerInput.responseBuilder
+      .speak(sessionAttributes.speakOutput)
+      .reprompt(sessionAttributes.repromptSpeech)
+      .getResponse();
+  },
+};
+
+const RepeatHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent';
+  },
+  handle(handlerInput) {
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+    return handlerInput.responseBuilder
+      .speak(sessionAttributes.speakOutput)
+      .reprompt(sessionAttributes.repromptSpeech)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const speakOutput = requestAttributes.t('STOP_MESSAGE', requestAttributes.t('SKILL_NAME'));
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    console.log("Inside SessionEndedRequestHandler");
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+/* CONSTANTS */
+const skillBuilder = Alexa.SkillBuilders.custom();
+const languageStrings = {
+  en: {
+    translation: {
+      RECIPES: recipes.RECIPE_EN_US,
+      SKILL_NAME: 'Delicious Fudge',
+      WELCOME_MESSAGE: 'Welcome to %s. You can ask a question like, what\'s the recipe for a %s? ... Now, what can I help you with?',
+      WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
+      DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
+      HELP_MESSAGE: 'You can ask questions such as, what\'s the recipe for a %s, or, you can say exit...Now, what can I help you with?',
+      HELP_REPROMPT: 'You can say things like, what\'s the recipe for a %s, or you can say exit...Now, what can I help you with?',
+      STOP_MESSAGE: 'Goodbye!',
+      RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
+      RECIPE_NOT_FOUND_MESSAGE: 'I\'m sorry, I dont really know ',
+      RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
+      RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
+      RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?'
+    },
+  },
+  'en-US': {
+    translation: {
+      RECIPES: recipes.RECIPE_EN_US,
+      SKILL_NAME: 'Delicious Fudge'
+    },
+  },
+  'en-GB': {
+    translation: {
+      RECIPES: recipes.RECIPE_EN_GB,
+      SKILL_NAME: 'Delicious Fudge'
+    },
+  },
+  de: {
+    translation: {
+      RECIPES: recipes.RECIPE_DE_DE,
+      SKILL_NAME: 'Delicious Fudge',
+      WELCOME_MESSAGE: 'Welcome to %s. You can ask a question like, what\'s the recipe for a %s? ... Now, what can I help you with?',
+      WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
+      DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
+      HELP_MESSAGE: 'You can ask questions such as, what\'s the recipe for a %s, or, you can say exit...Now, what can I help you with?',
+      HELP_REPROMPT: 'You can say things like, what\'s the recipe for a %s, or you can say exit...Now, what can I help you with?',
+      STOP_MESSAGE: 'Goodbye!',
+      RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
+      RECIPE_NOT_FOUND_MESSAGE: 'I\'m sorry, I dont really know ',
+      RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
+      RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
+      RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?'
+    },
+  },
+};
+
+// Finding the locale of the user
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+      resources: languageStrings,
+      returnObjects: true
+    });
+
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function (...args) {
+      return localizationClient.t(...args);
+    };
+  },
+};
+
+// getRandomItem
+function getRandomItem(arrayOfItems) {
+  // the argument is an array [] of words or phrases
+  let i = 0;
+  i = Math.floor(Math.random() * arrayOfItems.length);
+  return (arrayOfItems[i]);
+};
+
+/* LAMBDA SETUP */
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    RecipeHandler,
+    HelpHandler,
+    RepeatHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addRequestInterceptors(LocalizationInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ fudge-recipes/recipes.js
@@ -0,0 +1,29 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'RECIPE_EN_GB': {
+    'chocolate fudge': 'Chocolate fudge needs 400 grams chocolate, one can of condensed milk, 25 grams butter, 100 grams icing sugar, 55 grams of optional filling - for example, nuts, sweets, fruit.',
+    'vanilla fudge': 'Vanilla fudge requires one can of condensed milk, 150 millilitres of milk, 450 grams demerara sugar, 115 grams butter.',
+    'sea salted fudge': 'For salted fudge, you need one can of condensed milk, 80 grams of unsalted butter, 150 grams of soft light brown sugar, one whole vanilla pod, a good pinch of sea salt.',
+    'gingerbread fudge': 'For gingerbread fudge, take one can of condensed milk, 340 grams of white chocolate, 1 teaspoon of cinnamon, and 1 teaspoon of nutmeg.',
+    'boozy fudge': 'Boozy fudge needs 400 grams of golden caster sugar, one can of condensed milk, 140 grams salted butter, 1 teaspoon vanilla bean paste, 2 table spoons of alcohol - brandy, Baileys or amaretto work well!',
+    'christmas fudge': 'You need 400 grams of white caster sugar, 300 millilitres double cream, 1 teaspoon vanilla extract, 10 grams white chocolate and star sprinkles to decorate!',
+  },
+  'RECIPE_EN_US': {
+    'chocolate fudge': 'Chocolate fudge needs 400 grams chocolate, one can of condensed milk, 25 grams butter, 100 grams icing sugar, 55 grams of optional filling - for example, nuts, sweets, fruit.',
+    'vanilla fudge': 'Vanilla fudge requires one can of condensed milk, 150 millilitres of milk, 450 grams demerara sugar, 115 grams butter.',
+    'sea salted fudge': 'For salted fudge, you need one can of condensed milk, 80 grams of unsalted butter, 150 grams of soft light brown sugar, one whole vanilla pod, a good pinch of sea salt.',
+    'gingerbread fudge': 'For gingerbread fudge, take one can of condensed milk, 340 grams of white chocolate, 1 teaspoon of cinnamon, and 1 teaspoon of nutmeg.',
+    'boozy fudge': 'Boozy fudge needs 400 grams of golden caster sugar, one can of condensed milk, 140 grams salted butter, 1 teaspoon vanilla bean paste, 2 table spoons of alcohol - brandy, Baileys or amaretto work well!',
+    'christmas fudge': 'You need 400 grams of white caster sugar, 300 millilitres double cream, 1 teaspoon vanilla extract, 10 grams white chocolate and star sprinkles to decorate!',
+  },
+  'RECIPE_DE_DE': {
+    'chocolate fudge': 'Chocolate fudge needs 400 grams chocolate, one can of condensed milk, 25 grams butter, 100 grams icing sugar, 55 grams of optional filling - for example, nuts, sweets, fruit.',
+    'vanilla fudge': 'Vanilla fudge requires one can of condensed milk, 150 millilitres of milk, 450 grams demerara sugar, 115 grams butter.',
+    'sea salted fudge': 'For salted fudge, you need one can of condensed milk, 80 grams of unsalted butter, 150 grams of soft light brown sugar, one whole vanilla pod, a good pinch of sea salt.',
+    'gingerbread fudge': 'For gingerbread fudge, take one can of condensed milk, 340 grams of white chocolate, 1 teaspoon of cinnamon, and 1 teaspoon of nutmeg.',
+    'boozy fudge': 'Boozy fudge needs 400 grams of golden caster sugar, one can of condensed milk, 140 grams salted butter, 1 teaspoon vanilla bean paste, 2 table spoons of alcohol - brandy, Baileys or amaretto work well!',
+    'christmas fudge': 'You need 400 grams of white caster sugar, 300 millilitres double cream, 1 teaspoon vanilla extract, 10 grams white chocolate and star sprinkles to decorate!',
+  },
+};
