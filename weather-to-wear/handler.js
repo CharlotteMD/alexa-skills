39f5d0f02f843984963ae3fb36233d979db982c1
CharlotteMD <charlottemdavies@hotmail.co.uk>
2019-01-02 15:21:58 +0000

    fixed a bug that blocked the whole app - spelt succeed wrong - corrected and now basic app works - hello and launch intents

--- weather-to-wear/handler.js
+++ weather-to-wear/handler.js
@@ -12,7 +12,6 @@ exports.handler = function(event, context) {
 
             let options = {};
 
-            // options.speechText += getWish();
             options.speechText = "Hello. I can help you choose your outfit, based on the weather. Let's get started. Ask me, what shall I wear today?";
             options.repromptText = "Let me help you choose what to wear today.  Ask me, what shall I wear today?";
             options.endSession = false;
@@ -27,15 +26,7 @@ exports.handler = function(event, context) {
 
                 options.speechText = "Working. Get the weather. ";
 
-                // getWeather(function(forecast, err) {
-                //     if(err) {
-                //         context.fail(err);
-                //     } else { 
-                //         options.speechText += forecast;
-                //         options.endSession = true;
-                //         context.succeed(buildResponse(options));
-                //     }
-                // });
+                context.succeed(buildResponse(options));
 
             } else {
                 throw("Unknown intent")
@@ -50,45 +41,6 @@ exports.handler = function(event, context) {
         context.fail("Exception: " +e);
     }
 }
-
-// function getWeather() {
-    
-//     var url = 'https://api.darksky.net/forecast/63c7e1fe04debd05e2a196e39bc9e9c4/51.4570,0.2288?units=uk2';
-//     var req = http.get(url, function(res) {
-
-//         var body = "";
-
-//         res.on('data', function(chunk) {
-//             body += chunk;
-//         });
-
-//         res.on('end', function() {
-//             body = body.replace(/||/g, '');
-//             var forecast = JSON.parse(body);
-//             callback(forecast.data.currently.summary)
-//         });
-//     });
-
-//     req.on('error'. function(err) {
-//         callback(err);
-//     });
-// }
-
-// function getWish() {
-//     let myDate = new Date();
-//     let hours = myDate.getHours();
-
-//     if (hours < 0) {
-//         hours = hours + 24;
-//     }
-//     if (hours < 12) {
-//         return "Good Morning.";
-//     } else if (hours < 18) {
-//         return "Good Afternoon.";
-//     } else {
-//         return "Good evening";
-//     }
-// }
 
 
 function buildResponse(options) {
