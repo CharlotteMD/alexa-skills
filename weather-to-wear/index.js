9f311e0e9a4030bc182429c5e1c351e5c19f5149
CharlotteMD <charlottemdavies@hotmail.co.uk>
2019-01-02 11:29:38 +0000

    basic weather app written

--- weather-to-wear/index.js
+++ weather-to-wear/index.js
@@ -1,17 +1,63 @@
+'use strict';
+
 exports.handler = function(event, context) {
 
-    var request = event.request;
+    try {
 
-    if (request.type === "LaunchRequest") {
+        var request = event.request;
 
-    } else if (request.type === "IntentRequest") {
-    
-    } else if (request.type === "SessionEndedRequest") {
+        if (request.type === "LaunchRequest") {
 
-    } else {
-        context.fail("Unknown intent type");
+            let options = {};
+
+            options.speechText = "Good Morning. I can help you choose your outfit, based on the weather. Let's get started. Ask me, what shall I wear today?";
+            options.repromptText = "Let me help you choose what to wear today.  Ask me, what shall I wear today?";
+            options.endSession = false;
+
+            context.succeed(buildResponse(options));
+
+        } else if (request.type === "IntentRequest") {
+
+            let options = {};
+
+            if (request.intent.name === "HelloIntent") {
+                options.speechText = "Hello";
+                options.speechText += getWish();
+                options.endSession = true;
+
+                context.succeed(buildResponse(options));
+
+            } else {
+                throw("Unknown intent")
+            }
+        
+        } else if (request.type === "SessionEndedRequest") {
+
+        } else {
+            throw("Unknown intent type");
+        }
+    } catch(e) {
+        context.fail("Exception: " +e);
     }
 }
+
+function getWish() {
+    var myDate = new Date();
+    var hours = myDate.getHours();
+
+    if (hours < 0) {
+        hours = hours + 24;
+    }
+
+    if (hours < 12) {
+        return "Good Morning.";
+    } else if (hours < 18) {
+        return "Good Afternoon.";
+    } else {
+        return "Good evening";
+    }
+}
+
 
 function buildResponse(options) {
 
