b8f782c107b0efcec97079673704b4a6444e97c0
CharlotteMD <charlottemdavies@hotmail.co.uk>
2019-01-08 18:06:51 +0000

    completed basic adventure game

--- AdventureGame/src/app.js
+++ AdventureGame/src/app.js
@@ -26,16 +26,67 @@ app.use(
 
 app.setHandler({
     LAUNCH() {
-        this.toIntent('HelloWorldIntent');
+      let speech = 'Do you either go through the blue door, or through the red door?';
+      let reprompt = 'You have two options, the blue door, or the red door.';
+      this.ask(speech, reprompt);
     },
 
-    HelloWorldIntent() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    EnterDoorIntent() {
+      let speech = '';
+      let reprompt = '';
+
+      if (this.$inputs.color.value === 'blue') {
+        speech = 'You chose to go through the blue door. There is a dark corridor. Suddenly you hear a sound from the room at the end. Do you want to follow the sound?';
+        reprompt = 'Please say yes or no.'
+        this.followUpState('BlueDoorState').ask(speech, reprompt);
+      } else if (this.$inputs.color.value === 'red') {
+        speech = 'You chose to go through the red door. You find yourself in a small room with only one door, and a dog sleeping in front of it. To go through it, you would have to wake up the dog. Do you want to do it?';
+        reprompt = 'Please say yes or no.';
+        this.followUpState('RedDoorState').ask(speech, reprompt);
+      } else {
+        speech = 'Please choose either the blue door or the red door.';
+        reprompt = 'Say blue door, or red door.';
+        this.ask(speech, reprompt);
+      }
     },
 
-    MyNameIsIntent() {
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+    BlueDoorState: {
+    YesIntent() {
+        let speech = 'Blue Door: You chose Yes!';
+        this.tell(speech);
     },
+
+    NoIntent() {
+        let speech = 'Blue Door: You chose No!';
+        this.tell(speech);
+    },
+
+    Unhandled() {
+            this.followUpState('BlueDoorState')
+                .ask('You have to answer with yes or no.', 'Please say yes or no');
+        },
+},
+RedDoorState: {
+    YesIntent() {
+        let speech = 'Red Door: You chose Yes!';
+        this.tell(speech);
+    },
+
+    NoIntent() {
+        let speech = 'Red Door: You chose No!';
+        this.tell(speech);
+    },
+
+    Unhandled() {
+            this.followUpState('RedDoorState')
+                .ask('You have to answer with yes or no.', 'Please say yes or no');
+        },
+},
+
+Unhandled() {
+    this.toIntent('LAUNCH');
+},
+
 });
 
 module.exports.app = app;
